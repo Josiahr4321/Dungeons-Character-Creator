d13b775cc3fde2ca5d49e76016cea404e45a541e
mybaditssam <shernandez@econnect.eco>
2023-07-12 19:31:19 -0400

    Script created to console log API response

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
-function createCharacter() {
+createCharacter=() => {
   const name = process.argv[2];
   const selectedClass = process.argv[3];
   const selectedRace = process.argv[4]
-  const selectedalignment = 
+  const selectedSpell = process.argv[5]
+  const selectedEquipment = process.argv[6]
 
   fetch("https://www.dnd5eapi.co/api/classes/" + selectedClass)
     .then(function(response) {
@@ -21,16 +22,26 @@ function createCharacter() {
   .then(function(response) {
     return response.json();
   }).then(function(data) {
-    console.log("Race: " +data.name + " Alignment: " + data.alignment)
+    console.log("Race: " +data.name)
+    console.log("Alignment: " + data.alignment)
+    console.log("Language: "+data.language_desc)
   }).catch(function(er) {
     console.log(er)
   })
-
-  fetch("https://www.dnd5eapi.co/api/alignments/" + selectedAlignment)
+  fetch("https://www.dnd5eapi.co/api/spells/" + selectedSpell)
   .then(function(response) {
     return response.json();
   }).then(function(data) {
-    console.log("Race: " +data.name + " Alignment: " + data.alignment)
+    console.log("Spell: " + data.desc)
+  }).catch(function(er) {
+    console.log(er)
+  })
+  fetch("https://www.dnd5eapi.co/api/equipment/" + selectedEquipment)
+  .then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log("Equipment: " + data.index)
+    console.log("Weight: " + data.weight)
   }).catch(function(er) {
     console.log(er)
   })
