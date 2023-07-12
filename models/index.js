dc676df4ab7ce31e46e50338e2f4eef40ef77b48
Anthony-D99 <anthonydisaf@yahoo.com>
2023-07-12 19:00:12 -0400

    created connection between models in models/index.js also deleted seeds folder.

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Character = require('./Character');
+
+
+User.hasMany(Character, {
+    foreignKey:'user_id',
+});
+
+Character.belongsTo(User, {
+    foreignKey: 'user_id',
+});
