713761010848dd8b42bc804b7dffa55b49548c89
Anthony-D99 <anthonydisaf@yahoo.com>
2023-07-13 20:36:33 -0400

    fixed main.handlebars to display login

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 
-router.use('/api', apiRoutes);
+
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
 
 module.exports = router;
