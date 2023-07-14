addc789e5e337512e3dd6233687c5566e99e29ad
Anthony-D99 <anthonydisaf@yahoo.com>
2023-07-14 14:54:28 -0400

    test

--- server.js
+++ server.js
@@ -42,47 +42,47 @@ sequelize.sync({ force: false }).then(() => {
  //app.use(bodyParser.json());
 
 // Set up Handlebars as the template engine
- app.engine('handlebars', exphbs());
- app.set('view engine', 'handlebars');
+//  app.engine('handlebars', exphbs());
+//  app.set('view engine', 'handlebars');
 
-// Store created characters
- let characters = [];
+// // Store created characters
+//  let characters = [];
 
-// Handle character creation form submission
- app.post('/create-character', (req, res) => {
-   const character = {
-     race: req.body.race,
-     class: req.body.class,
-     spells: req.body.spells,
-     equipment:req.body.equipment,
-   };
+// // Handle character creation form submission
+//  app.post('/create-character', (req, res) => {
+//    const character = {
+//      race: req.body.race,
+//      class: req.body.class,
+//      spells: req.body.spells,
+//      equipment:req.body.equipment,
+//    };
 
-   characters.push(character);
+//    characters.push(character);
 
-//   // You can perform additional logic or validation here
+// //   // You can perform additional logic or validation here
 
-   res.json({ message: 'Character created successfully!' });
- });
+//    res.json({ message: 'Character created successfully!' });
+//  });
 
- fetch('')
-   .then(response => response.json())
-   .then(data => {
-     const races = data.races;
-   });
+//  fetch('')
+//    .then(response => response.json())
+//    .then(data => {
+//      const races = data.races;
+//    });
 
 
-// Render the homepage view
- app.get('/', (req, res) => {
-   res.render('homepage', { title: 'Homepage' });
- });
+// // Render the homepage view
+//  app.get('/', (req, res) => {
+//    res.render('homepage', { title: 'Homepage' });
+//  });
 
-// Render the login view
- app.get('/login', (req, res) => {
-   res.render('login', { title: 'Log In' });
- });
+// // Render the login view
+//  app.get('/login', (req, res) => {
+//    res.render('login', { title: 'Log In' });
+//  });
 
-// Start the server
- //app.listen(port, () => {
-   //console.log(`Server running on port ${port}`);
-   //connect();
- //});
+// // Start the server
+//  //app.listen(port, () => {
+//    //console.log(`Server running on port ${port}`);
+//    //connect();
+//  //});
