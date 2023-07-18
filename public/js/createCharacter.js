67ea8e24820a5ccaed911ca2b7488568e1877c6c
Anthony-D99 <anthonydisaf@yahoo.com>
2023-07-18 14:29:02 -0400

    added delete character function, deleted the character details handlebars and moved code over to homepage. added script for nodemon package, and cleaned up routes

--- public/js/createCharacter.js
+++ public/js/createCharacter.js
@@ -1,17 +1,17 @@
 const createCharacterFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-create').value.trim();
-    const race = document.querySelector('#race-create').value.trim();
-    const clas = document.querySelector('#class-create').value.trim();
-    const alignment = document.querySelector('#alignment-create').value.trim();
-    const spells = document.querySelector('#spells-create').value.trim();
-    const equipment = document.querySelector('#equipment-create').value.trim();
-
+    const name = document.querySelector('.name-create').value.trim();
+    const race = document.querySelector('.race-create').value.trim();
+    const clas = document.querySelector('.class-create').value.trim();
+    const alignment = document.querySelector('.alignment-create').value.trim();
+    const spells = document.querySelector('.spells-create').value.trim();
+    const equipment = document.querySelector('.equipment-create').value.trim();
+    
     if (name && race && clas && alignment && spells && equipment) {
       const response = await fetch('/api/characters', {
         method: 'POST',
-        body: JSON.stringify({ name, race, alignment, spells, equipment }),
+        body: JSON.stringify({ name, race, clas, alignment, spells, equipment }),
         headers: { 'Content-Type': 'application/json' },
       });
       console.log(response)
