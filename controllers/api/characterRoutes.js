2ea72de6bd22e097f0795b48f36b79daf99b87e3
mybaditssam <shernandez@econnect.eco>
2023-07-20 13:58:12 -0400

    Created functionality which depending on the class, an image will be displayed, also some css styling

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -1,58 +1,59 @@
 const router = require('express').Router();
 const { Character } = require('../../models');
+const path = require('path');
 
 router.get('/', async (req, res) => {
   try {
-      const characterData = await  Character.findAll({
-          attributes: { exclude: ['user_id'] },
-          order: [['name', 'ASC']],
-          where: {user_id: req.session.user_id}
-      });
+    const characterData = await Character.findAll({
+      attributes: { exclude: ['user_id'] },
+      order: [['name', 'ASC']],
+      where: { user_id: req.session.user_id }
+    });
 
-      const characters = characterData.map((character) => character.get({ plain: true }));
+    const characters = characterData.map((character) => character.get({ plain: true }));
 
-      res.render('homepage', {
-          characters,
-          loggedIn: req.session.loggedIn,
-      });
+    res.render('homepage', {
+      characters,
+      loggedIn: req.session.loggedIn,
+    });
   } catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
 router.post('/', async (req, res) => {
-    try {
-      const newCharacter = await Character.create({
-        ...req.body,
+  try {
+    console.log('Request body:', req.body);
+    const newCharacter = await Character.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newCharacter);
+  } catch (err) {
+    console.error('Error creating character:', err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const characterData = await Character.destroy({
+      where: {
+        id: req.params.id,
         user_id: req.session.user_id,
-      });
-  console.log(newCharacter)
-      res.status(200).json(newCharacter);
-    } catch (err) {
-      res.status(400).json(err);
+      },
+    });
+
+    if (!characterData) {
+      res.status(404).json({ message: 'No character found with this id!' });
+      return;
     }
-  });
-  
-  router.delete('/:id', async (req, res) => {
-    console.log(req.params.id, req.session.user_id)
-    try {
-      console.log(req.params.id)
-      const characterData = await Character.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!characterData) {
-        res.status(404).json({ message: 'No character found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(characterData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  module.exports = router;
+
+    res.status(200).json(characterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
